0f24c0d0621aa77e8214f643e801b3cf825df8b6
Nicolas <nicolasalcaraz@gmail.com>
2024-09-12 00:37:44 -0300

    ejercicios en array

---
+++ ej_arrays.md
@@ -0,0 +1,30 @@
+Ejercicios Clase 3 con Js
+
+Nota: Crear un repositorio y versionar cada ejercicio mediante un commit con el
+mensaje que considere mas claro. Crear un repositorio remoto y sincronizar el local
+con el mismo.
+
+Ejercicio 1: Filtrar personajes menores de edad
+Crear un array de objetos con información de personajes de Los Simpsons, cada
+objeto debe contener el nombre, la edad y el rol del personaje. Utilizar el método
+filter para obtener un nuevo array que contenga solamente los personajes
+menores de 18 años.
+
+Ejercicio 2: Sumar edades de los personajes
+Utilizar el método reduce para sumar las edades de todos los personajes del array
+creado en el Ejercicio 1. Mostrar el resultado en la consola.
+
+Ejercicio 3: Obtener los nombres de los personajes
+Utilizar el método map y una función arrow para crear un nuevo array que contenga
+solamente los nombres de los personajes del array del Ejercicio 1. Mostrar el nuevo
+array en la consola.
+
+Ejercicio 4: Cambiar el rol de los personajes
+Utilizar el método map y destructuring para crear un nuevo array a partir del array
+del Ejercicio 1, en el cual el rol de los personajes menores de edad sea cambiado a
+"Estudiante". Mostrar el nuevo array en la consola.
+
+Ejercicio 5: Agregar personajes con spread
+Crear un nuevo array de objetos con más personajes de Los Simpsons. Utilizar el
+operador spread para combinar ambos arrays de personajes en un nuevo array.
+Mostrar el array resultante en la consola.
--- js/index.js
+++ js/index.js
@@ -1,5 +1,46 @@
-document.getElementById("form-container").addEventListener('submit',function(event){
-    event.preventDefault()
-    alert("Formulario Enviado!")
-})
+const personajesSimpsons = [
+    { nombre: "Homero", edad: 39, rol: "Padre" },
+    { nombre: "Marge", edad: 36, rol: "Madre" },
+    { nombre: "Bart", edad: 10, rol: "Hijo" },
+    { nombre: "Lisa", edad: 8, rol: "Hija" },
+    { nombre: "Maggie", edad: 1, rol: "Hija" },
+    { nombre: "Ned", edad: 35, rol: "Vecino" },
+    { nombre: "Ralph", edad: 8, rol: "Compañero de clase" },
+    { nombre: "Milhouse", edad: 10, rol: "Amigo" },
+    { nombre: "Nelson", edad: 12, rol: "Bravucón" },
+    { nombre: "Martin", edad: 10, rol: "Compañero de clase" }
+    ];
 
+//ej1 
+const personajesMenores = personajesSimpsons.filter((personaje) => personaje.edad < 18)
+personajesMenores.forEach((p) => console.log(p));
+
+
+/*ej2
+const suma = personajesSimpsons.reduce((acumulador,valorActual) => acumulador + valorActual.edad, 0);
+console.log(suma)
+*/
+
+/*ej3
+const nombres = personajesSimpsons.map((personaje) => personaje.nombre)
+nombres.forEach((nombre)=>console.log(nombre))
+*/
+
+
+console.log("--------------ejercicio 5----------------------")
+const nuevosPersonajes = personajesMenores.map(({ nombre, edad, rol }) => {
+    return { nombre, edad, rol: "Estudiante" }; 
+});
+
+nuevosPersonajes.forEach((p) => console.log(p));
+
+console.log("--------------ejercicio 6----------------------")
+
+const otrosPersonajes = [
+    { nombre: "Carl", edad: 39, rol: "Amigo de Homero" },
+    { nombre: "Lenny", edad: 45, rol: "Amigo de homero" },
+    { nombre: "Moe", edad: 52, rol: "Amigo de Homero" },
+]
+
+const combiado = [...personajesSimpsons,...otrosPersonajes]
+console.log(combiado)
